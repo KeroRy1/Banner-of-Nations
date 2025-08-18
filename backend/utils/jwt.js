613f32749b747b82c3234888d2bf0c7c902493a6
KeroRy1 <42krmkrmn07@gmail.com>
2025-08-18 22:07:39 +0300

    Delete backend directory

--- backend/utils/jwt.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken')
-const SECRET = process.env.JWT_SECRET || 'devsecret'
-module.exports = {
-  sign: (payload, opts={}) => jwt.sign(payload, SECRET, {...opts, expiresIn:'7d'}),
-  verify: (token)=> jwt.verify(token, SECRET)
-}
-
