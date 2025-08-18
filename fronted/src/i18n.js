5f25645bafd55deac55588a0e081296cf31c28a8
KeroRy1 <42krmkrmn07@gmail.com>
2025-08-18 19:49:22 +0300

    Update i18n.js

--- fronted/src/i18n.js
+++ fronted/src/i18n.js
@@ -1,1 +1,16 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+const resources = {
+  en: { translation: { "hero_title": "Banner of Nations", "get_started":"Get started" } },
+  tr: { translation: { "hero_title": "Banner of Nations", "get_started":"Başla" } }
+}
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'tr',
+  fallbackLng: 'en',
+  interpolation: { escapeValue: false }
+})
+export default i18n
 
