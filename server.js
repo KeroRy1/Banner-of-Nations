d5e474132081fec9f3b3f068648c6cd37398c98e
KeroRy1 <42krmkrmn07@gmail.com>
2025-08-18 23:14:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ app.get('/googleaa1a9047867b85a7.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'googleaa1a9047867b85a7.html'));
 });
 
-});
-
 // Port ayarı
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
