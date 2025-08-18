5b842d502fa0cee54c6b50700f63caa2eb366e7c
KeroRy1 <42krmkrmn07@gmail.com>
2025-08-18 19:51:24 +0300

    Update jwt.js

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,1 +1,7 @@
+const jwt = require('jsonwebtoken')
+const SECRET = process.env.JWT_SECRET || 'devsecret'
+module.exports = {
+  sign: (payload, opts={}) => jwt.sign(payload, SECRET, {...opts, expiresIn:'7d'}),
+  verify: (token)=> jwt.verify(token, SECRET)
+}
 
